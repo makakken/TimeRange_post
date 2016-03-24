001e4bfcda909c1413f8e38c3035564e058259f9
Stephan <admin.l@peac.me>
2016-03-24 12:42:40 +0100

    added dashicons-calendar to datepicker
    changed to german date format
    
    debugged

--- TimeRange_post.php
+++ TimeRange_post.php
@@ -1,4 +1,17 @@
 <?php
+/*
+Plugin Name: TimeRange - Post
+Plugin URI:  http://makakken.de/wordpress/timerangepost
+Description: This Plugin adds Start/End - Timestamps as Post-Meta to Posts.
+Version:     0.0.1
+Author:      Stephan Kropop
+Author URI:  http://makakken.de
+License:     GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.de.html
+Domain Path: /languages
+Text Domain: timerange-post
+*/
+
 class TimeRange_post {
 
 	private $version = '0.0.1';
@@ -7,16 +20,17 @@ class TimeRange_post {
 			add_meta_box(
 				'TimeRange_metabox',          // this is HTML id of the box on edit screen
 				__('Ausstellungsdaten'),    // title of the box
-				'TimeRange_post',   // function to be called to display the checkboxes, see the function below
-				'event',        // on which edit screen the box should appear
+				array('TimeRange_post','TimeRange_MetaBox'),   // function to be called to display the checkboxes, see the function below
+				'post',        // on which edit screen the box should appear
 				'advanced',      // part of page where the box should appear
 				'high'      // priority of the box
 			);
 	}
 
-	public static function TimeRange_post($post) {
+	public static function TimeRange_MetaBox($post) {
 		wp_enqueue_script('jquery-ui-datepicker',false,array('jquery') );
 		wp_enqueue_style( 'timerange-post',plugins_url('style.css', __FILE__));
+		wp_enqueue_script( 'timerange-post-dateppicker',plugins_url('js/TimeRange-Post.js', __FILE__));
 
 		// Set Default Dates
 		$start_date = $end_date = date('Y-m-d', time());
@@ -27,7 +41,7 @@ class TimeRange_post {
 		if( intval(get_post_meta($post->ID,'event_end_date',true)) > 0 ) {
 			$end_date = date('Y-m-d',intval(get_post_meta($post->ID,'event_end_date',true)));
 		}
-
+		echo '<div class"TimeRange-Post">';
 		echo '<table>'.PHP_EOL;				  
 		echo ' <tr>'.PHP_EOL;
 		echo '	<td> '.PHP_EOL;
@@ -42,11 +56,7 @@ class TimeRange_post {
 		echo '  <td><input type="date" name="event_end_date" class="date" value="'.$end_date.'"/></td>'.PHP_EOL;
 		echo ' </tr>'.PHP_EOL;
 		echo '</table>'.PHP_EOL;
-		echo '<script language="javascript">';
-		echo 'jQuery(function() {';
-		echo ' if ( jQuery(\'[type="date"]\').prop(\'type\') != "date" ) { jQuery(\'[type="date"]\').datepicker({ dateFormat: \'dd.mm.yy\' }); }';
-		echo '});';
-		echo '</script>';
+		echo '</div>';
 	}
 }
 
---
+++ js/TimeRange-Post.js
@@ -0,0 +1,26 @@
+jQuery(function() {
+  if ( jQuery('[type="date"]').prop('type') != "date" ) { 
+      jQuery('[type="date"]').datepicker({
+        prevText: '&#x3c;zurück', prevStatus: '',
+        prevJumpText: '&#x3c;&#x3c;', prevJumpStatus: '',
+        nextText: 'Vor&#x3e;', nextStatus: '',
+        nextJumpText: '&#x3e;&#x3e;', nextJumpStatus: '',
+        currentText: 'heute', currentStatus: '',
+        todayText: 'heute', todayStatus: '',
+        clearText: '-', clearStatus: '',
+        closeText: 'schließen', closeStatus: '',
+        monthNames: ['Januar','Februar','März','April','Mai','Juni',
+        'Juli','August','September','Oktober','November','Dezember'],
+        monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
+        'Jul','Aug','Sep','Okt','Nov','Dez'],
+        dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+        dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+        dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+        buttonText: "<span class='dashicons dashicons-calendar-alt'></span>",
+      showMonthAfterYear: false,
+      showOn: 'both',
+      dateFormat:'d MM, y'
+    });
+	jQuery('#ui-datepicker-div').addClass('TimeRange-Post');
+  }
+});
